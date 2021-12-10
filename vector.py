103ec84492f2d9e93ed814346b1563ce31458e07
Ndeye Fatou <ndeyefatousene1502@gmail.com>
2021-12-10 18:57:31 +0000

    Renommer la fonction norme

--- vector.py
+++ vector.py
@@ -3,9 +3,6 @@ class Vecteur:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-    def longueur(self):
+    def norme(self):
         return math.sqrt(self.x**2 + self.y**2)
-vect1 = Vecteur(2, 5)
-print(vect1.longueur())
-
     
